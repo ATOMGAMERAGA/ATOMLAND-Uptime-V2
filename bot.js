c37db759723d15a64c9853d4cb17e940b53ae3b3
ATOMGAMERAGA <atomlandtowny@gmail.com>
2024-12-29 12:34:10 +0300

    gg

--- bot.js
+++ bot.js
@@ -2,15 +2,23 @@
 
 require("dotenv").config();
 
-const { getConfig, checkConfig } = require("./functions/config");
-checkConfig();
-const { token } = getConfig();
 const { client } = require("./client");
-
 require("moment-duration-format");
 require("./slash")(client);
 
+// Çevresel değişkenlerden tokeni al
+const token = process.env.TOKEN;
+
+// Tokenin mevcut olup olmadığını kontrol et
+if (!token) {
+  console.error("Hata: TOKEN değişkeni .env dosyasında bulunamadı.");
+  process.exit(1); // Programı hatayla sonlandır
+}
+
+// Hata işleyicileri
 process
   .on("uncaughtException", console.error)
   .on("unhandledRejection", console.error);
+
+// Botu başlat
 client.login(token);
